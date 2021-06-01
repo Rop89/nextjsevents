26e7d8fd25cf5f498d2518878a024b58819b1fa9
Rita <ritaa.pinho@gmail.com>
2021-06-01 10:37:53 +0100

    initial commit

---
+++ pages/events/[eventId].js
@@ -0,0 +1,32 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+import { getEventById } from "../../dummy-data";
+import ErrorAlert from "../../components/ui/error-alert";
+
+export default function EventDetailPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <ErrorAlert>{event.description}</ErrorAlert>
+      </EventContent>
+    </Fragment>
+  );
+}
